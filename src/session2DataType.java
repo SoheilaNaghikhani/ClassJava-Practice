8886f77864dfa5dff4ca32abe4e6aeba898cad4b
soheila naqikhani <soheila@gmail.com>
2024-05-27 20:01:21 +0330

    commit1

---
+++ src/session2DataType.java
@@ -0,0 +1,24 @@
+package src;
+
+public class session2DataType {
+    public static void main(String[] args) {
+        System.out.println("Information of a Dummy Student : ");
+        char thefirstLetterOfName='F';
+        String firstName="Fatemeh";
+        String lastName="mahmoudi";
+        byte age=18;
+        int height=168;
+        boolean female=true;
+        long priceCar=85000000000L;
+        float saraly=31124235.124587f;
+        double insurance=3112423.5124587;
+
+        System.out.println("The First Letter Of Name : "+ thefirstLetterOfName );
+        System.out.println("Name and Family is : "+firstName + " "+lastName);
+        System.out.println("Age is : "+ age +", Height is " + height);
+        System.out.println("Student is Female : "+ female);
+        System.out.println("Salary is : " + saraly);
+        System.out.println("Insurance Of Salary is : " + insurance);
+
+    }
+}
